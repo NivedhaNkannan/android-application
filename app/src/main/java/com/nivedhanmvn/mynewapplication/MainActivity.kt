6199dc6b52a7d5dbd0fe3721209cf91b6c0d9176
nived <nivedhaa.kannan@gmail.com>
2024-07-27 11:16:57 +0530

    project

---
+++ app/src/main/java/com/nivedhanmvn/mynewapplication/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.nivedhanmvn.mynewapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main_relative_layout)
+        title="THE AVENGERS"
+    }
+    }
